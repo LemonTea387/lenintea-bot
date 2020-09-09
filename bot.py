c85034e3e3bdfe5aa2ad1752025658272f2c560b
LemonTea387 <JunJieLow08@gmail.com>
2020-09-09 18:06:03 +0800

    First commit to add all bot.py code and requirements.txt

---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord
+import boto3
+
+client = discord.Client() #connect to discord client
+instance = boto3.client('ec2',region_name = 'ap-southeast-1')
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!') #tell console its connected to discord
+
+
+@client.event   #for event a.k.a things going on in discord
+async def on_message(message): #message stuff
+    if message.author == client.user: #prevent bot for repeating it self
+        return
+    if message.content == '+start': 
+        state = getInstanceState()
+        if state != 'running' and state != 'pending' and state != 'stopping':
+            turnOnInstance()
+            await message.channel.send("Server is starting")
+        else:
+            await message.channel.send('Server is ' + state + '\nIf you think this is a bug, contact your incompetent developer.')
+    elif message.content == '+state':
+            await message.channel.send('AWS Instance state : ' + getInstanceState())
+
+#functions to start instance
+def turnOnInstance():
+    try:
+        response = instance.start_instances(
+        InstanceIds=[
+            INSTANCE_ID
+                ],
+        DryRun = False
+        )
+        return True
+    except:
+        return False
+
+def getInstanceState():
+    try:
+        response = instance.describe_instance_status(InstanceIds=[INSTANCE_ID])
+        return response['InstanceStatuses'][0]['InstanceState']['Name']
+    except:
+        return 'Server is not up :)'
+    
+client.run(TOKEN)
