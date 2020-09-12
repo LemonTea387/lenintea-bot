659cd4ab88e03eae8e98ad0b13ed481579f3b428
LemonTea387 <JunJieLow08@gmail.com>
2020-09-12 17:42:27 +0800

    Documented and formatted code for better readibility

--- bot.py
+++ bot.py
@@ -9,26 +9,38 @@ instance = boto3.client('ec2',region_name = os.getenv('REGION'),
                         aws_access_key_id = os.getenv('ACCESS_KEY'),
                         aws_secret_access_key = os.getenv('SECRET_KEY'))
 
+# On event of bot going up and running
 @client.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!') #tell console its connected to discord
+    print(f'{client.user.name} has come online and connected to Discord')
 
 
-@client.event   #for event a.k.a things going on in discord
-async def on_message(message): #message stuff
-    if message.author == client.user: #prevent bot for repeating it self
+@client.event
+async def on_message(message): 
+    # Return program flow when author is bot. Stops looping
+    if message.author == client.user: 
         return
+
     if message.content == '+start': 
         state = getInstanceState()
+        # Innitiate server only when instance state is not running, pending or stopping. 
+        # (AWS boto3 does not reply 'stopped' state)
         if state != 'running' and state != 'pending' and state != 'stopping':
-            turnOnInstance()
-            await message.channel.send("Server is starting")
+            if (turnOnInstance()):
+                await message.channel.send("Server is starting")
+            else:
+                await message.channel.send('Error in starting server')
+
         else:
             await message.channel.send('Server is ' + state + '\nIf you think this is a bug, contact your incompetent developer.')
-    elif message.content == '+state':
-            await message.channel.send('AWS Instance state : ' + getInstanceState())
 
-#functions to start instance
+    # Displays the state of the instance
+    elif message.content == '+state':
+        await message.channel.send('AWS Instance state : ' + getInstanceState())
+
+
+# Functions to start instance
+# Returns boolean on outcome
 def turnOnInstance():
     try:
         instance.start_instances(
@@ -41,6 +53,7 @@ def turnOnInstance():
     except:
         return False
 
+# Returns state of server as String
 def getInstanceState():
     try:
         response = instance.describe_instance_status(InstanceIds=[INSTANCE_ID])
