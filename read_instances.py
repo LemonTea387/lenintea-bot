4ac1c47db1b263d74e798f067a64ae6dcc3ff7ee
LemonTea387 <JunJieLow08@gmail.com>
2021-06-16 01:47:39 +0800

    Rewrote bot to use new format with discord.ext and cogs system.

--- read_instances.py
+++
@@ -1,6 +0,0 @@
-def get_instances():
-    import os
-    instances = {}
-    instances = os.environ
-    instances = {key:instances[key] for key in list(instances) if key.startswith("INSTANCE_ID")}
-    return instances
